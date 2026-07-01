b20930fe61c3f3821fbda9e9b4df4c06f2fac5fc
Diego Villanueva <dv270992@gmail.com>
2026-07-01 18:53:39 +0200

    chore(projects): update image extensions to jpg and rename projects

--- src/helpers/projectsArrays.ts
+++ src/helpers/projectsArrays.ts
@@ -8,11 +8,11 @@ export interface Project {
 
 export const projectsList: Project[] = [
   // Web
-  { id: 1, img: "/img/projects/webs/amaia-web.png", name: "Amaia Web", url: "https://amaia-gold-front.vercel.app", category: "web" },
-  { id: 2, img: "/img/projects/webs/burger-web.png", name: "Burger Web", url: "https://burger-store-mu.vercel.app", category: "web" },
-  { id: 3, img: "/img/projects/webs/coffee-web.png", name: "Coffee Web", url: "https://coffee-store-sepia-phi.vercel.app", category: "web" },
-  { id: 4, img: "/img/projects/webs/gym-web.png", name: "Gym Web", url: "https://gym-store-beige.vercel.app", category: "web" },
-  { id: 5, img: "/img/projects/webs/herbs-web.png", name: "Herbs Web", url: "https://herbs-web.vercel.app", category: "web" },
-  { id: 6, img: "/img/projects/webs/medics-web.png", name: "Medics Web", url: "https://medics-store.vercel.app", category: "web" },
-  { id: 7, img: "/img/projects/webs/ormigga-web.png", name: "Ormigga", url: "https://ormigga.com", category: "web" },
+  { id: 1, img: "/img/projects/webs/amaia-web.jpg", name: "Jewelry Web", url: "https://amaia-gold-front.vercel.app", category: "web" },
+  { id: 2, img: "/img/projects/webs/burger-web.jpg", name: "Burger Web", url: "https://burger-store-mu.vercel.app", category: "web" },
+  { id: 3, img: "/img/projects/webs/coffee-web.jpg", name: "Coffee Web", url: "https://coffee-store-sepia-phi.vercel.app", category: "web" },
+  { id: 4, img: "/img/projects/webs/gym-web.jpg", name: "Gym Web", url: "https://gym-store-beige.vercel.app", category: "web" },
+  { id: 5, img: "/img/projects/webs/herbs-web.jpg", name: "Herbs Web", url: "https://herbs-web.vercel.app", category: "web" },
+  { id: 6, img: "/img/projects/webs/medics-web.jpg", name: "Medics Web", url: "https://medics-store.vercel.app", category: "web" },
+  // { id: 7, img: "/img/projects/webs/ormigga-web.jpg", name: "Ormigga", url: "https://ormigga.com", category: "web" },
 ];
